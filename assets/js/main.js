408c770dfd40fa5eec8a86c6ea4f4055f19ccae5
FahadApplicon97 <fahad.masood@appliconsoft.com>
2026-06-12 19:53:18 +0500

    Announcement Bar Section Created

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,13 @@ class SiteHeader extends HTMLElement {
   }
 }
 customElements.define("site-header", SiteHeader);
+
+class SiteAnnouncementBar extends HTMLElement {
+  async connectedCallback() {
+    const html = await fetch("components/Common/announcement-bar.html").then((r) =>
+      r.text(),
+    );
+    this.innerHTML = html;
+  }
+}
+customElements.define("site-announcement-bar", SiteAnnouncementBar);
